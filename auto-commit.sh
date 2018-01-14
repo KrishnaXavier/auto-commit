4e68c9b3b1d4bb45f2644846c882d834672ffe59
KrishnaXavier <tsixav@gmail.com>
2018-01-14 01:19:48 -0200

    Data: 2018/01/14 - Hora: 01:19:48

--- auto-commit.sh
+++ auto-commit.sh
@@ -4,6 +4,6 @@ git add .
 
 git status
 
-git commit -a -m $1
+git commit -a -m "$1"
 
 git push origin master
--- loop-auto-commit.sh
+++ loop-auto-commit.sh
@@ -3,11 +3,11 @@
 while :
 do
 
-	sleep $1
-
 	d=$(date +%Y/%m/%d)
 	h=$(date +%T)
 
 	source auto-commit.sh "Data: $d - Hora: $h"
+
+	sleep $1
 
 done
